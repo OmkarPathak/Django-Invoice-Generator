73df72f2d609168ea059f4b14414570f473c4882
Omkar Pathak <omkarpathak27@gmail.com>
2018-07-24 19:13:36 +0530

    Added HSC Number to all templates and revampled admin panel

--- inventory_management/works/models.py
+++ inventory_management/works/models.py
@@ -20,11 +20,34 @@ class Challan(models.Model):
 class ChallanNumber(models.Model):
 	challan_number = models.IntegerField()
 
+	def __str__(self):
+		return str(self.challan_number)
+
+class HSCNumber(models.Model):
+	hsc_number  = models.CharField(max_length=200)
+	cgst 		= models.FloatField(('CGST (in %)'))
+	sgst 		= models.FloatField(('SGST (in %)'))
+	date_added	= models.DateTimeField(auto_now_add=True)
+
 class Report(models.Model):
-	invoice_of 	= models.CharField(max_length=200, null=True, blank=True)
-	challan 	= models.OneToOneField(Challan, null=True,blank=True, on_delete=models.CASCADE)
+	challan_number 	= models.IntegerField()
+	date			= models.DateField()
+	hsc_number		= models.CharField(max_length=200)
+	cgst			= models.FloatField()
+	sgst			= models.FloatField()
+	amount			= models.FloatField()
 
 class AddWorkForm(ModelForm):
 	class Meta:
 		model  = Work
 		fields = ['code', 'name', 'amount']
+
+class AddHSCForm(ModelForm):
+	class Meta:
+		model  = HSCNumber
+		fields = ['hsc_number', 'cgst', 'sgst']
+
+class AddChallanForm(ModelForm):
+	class Meta:
+		model  = ChallanNumber
+		fields = ['challan_number']
--- inventory_management/works/views.py
+++ inventory_management/works/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Work, AddWorkForm
+from .models import Work, AddWorkForm, HSCNumber, AddHSCForm, ChallanNumber, AddChallanForm
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
@@ -71,16 +71,41 @@ def get_code_values(request):
         }
         return JsonResponse(data)
 
+def get_hsc_values(request):
+    if request.method == 'GET':
+        code = request.GET.get('code')
+        hsc = HSCNumber.objects.get(hsc_number=code)
+        data = {
+            'id'  : hsc.id,
+            'cgst': hsc.cgst,
+            'sgst': hsc.sgst
+        }
+        return JsonResponse(data)
+
 def homepage(request):
     return render(request, 'base.html')
 
 def invoice_generator_melt(request):
+    challan_number = ChallanNumber.objects.get(id=1)
     works = Work.objects.all().order_by('code')
-    return render(request, 'invoice.html', {'works': works})
+    hsc   = HSCNumber.objects.all()
+    context = {
+        'works'         : works, 
+        'hsc'           : hsc,
+        'challan_number': challan_number
+    }
+    return render(request, 'invoice.html', context)
 
 def invoice_generator_assembly(request):
+    challan_number = ChallanNumber.objects.get(id=1)
     works = Work.objects.all().order_by('code')
-    return render(request, 'invoice_assembly.html', {'works': works})
+    hsc   = HSCNumber.objects.all()
+    context = {
+        'works'         : works, 
+        'hsc'           : hsc,
+        'challan_number': challan_number
+    }
+    return render(request, 'invoice_assembly.html', context)
 
 def admin(request):
     '''
@@ -91,7 +116,7 @@ def admin(request):
     if form.is_valid():
         form.save()
         messages.success(request, 'New Entry Added successfully')
-        form = AddWorkForm()
+        return redirect('admin')
     works = Work.objects.all().order_by('-date_added')
     return render(request, 'admin.html', {'form': form, 'works': works})
 
@@ -122,3 +147,72 @@ def admin_delete(request, id):
     work.delete()
     messages.success(request, 'Entry Deleted successfully')
     return redirect('admin')
+
+def hsc(request):
+    '''
+        To add new HSC entry.
+        User can add a new HSC Code with respective CGST and SGST amounts
+    '''
+    form = AddHSCForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'New Entry Added successfully')
+        return redirect('hsc')
+    hsc = HSCNumber.objects.all()
+    return render(request, 'hsc.html', {'form': form, 'hsc': hsc})
+
+def hsc_edit(request, id):
+    '''
+        To edit a single hsc code entry that is already created
+    '''
+    hsc = get_object_or_404(HSCNumber, id=id)
+    if request.method == 'POST':
+        form = AddHSCForm(request.POST, instance=hsc)
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'Entry Edited successfully')
+        return redirect('hsc')
+    else:
+        form = AddHSCForm(initial={
+                                'hsc_number'  : hsc.hsc_number,
+                                'cgst'        : hsc.cgst,
+                                'sgst'        : hsc.sgst 
+                            }, instance=hsc)
+        return render(request, 'hsc_edit_modal.html', {'form': form})
+        
+def hsc_delete(request, id):
+    '''
+        To delete a single entry already created
+    '''
+    hsc = get_object_or_404(HSCNumber, id=id)
+    hsc.delete()
+    messages.success(request, 'Entry Deleted successfully')
+    return redirect('hsc')
+
+def challan_no_edit(request, id):
+    '''
+        To edit current entry of challan number that is already created
+    '''
+    challan = get_object_or_404(ChallanNumber, id=id)
+    if request.method == 'POST':
+        form = AddChallanForm(request.POST, instance=challan)
+        if form.is_valid():
+            form.save()
+        messages.success(request, 'Entry Edited successfully')
+        return redirect('challan_no')
+    else:
+        form = AddChallanForm(initial={
+                                'challan_number'  : challan.challan_number
+                            }, instance=challan)
+        return render(request, 'challan_edit_modal.html', {'form': form})
+
+def challan_no(request):
+    '''
+        Get Current Challan Number
+    '''
+    try:
+        challan_number = ChallanNumber.objects.get(id=1)
+    except ChallanNumber.DoesNotExist:
+        ChallanNumber(challan_number=1).save()
+        challan_number = ChallanNumber.objects.get(id=1)
+    return render(request, 'challan.html', {'challan_number': challan_number})
